2e0c225a75a075e7621ce07e986979e5a1095cd0
LiuZijian <lzj@123>
2019-06-08 22:44:54 +0800

    add

---
+++ plugins/vue-plugin.js
@@ -0,0 +1,36 @@
+(function () {
+
+    const MyPlugin = {};
+    
+    MyPlugin.install = function (Vue, options) {
+        // 1. 添加全局方法或属性
+        Vue.myGlobalMethod = function () {
+            console.log("Vue函数对象的方法");
+        };
+
+        // 2. 添加全局资源
+        Vue.directive('my-directive', {
+            bind (el, binding, vnode, oldVnode) {
+                el.textContent = binding.value.toUpperCase();
+            }
+        });
+
+        // 3. 注入组件选项
+        Vue.mixin({
+            created: function () {
+                // 逻辑...
+            }
+        });
+
+        // 4. 添加实例方法
+        Vue.prototype.$myMethod = function (methodOptions) {
+            console.log("Vue实例对象的方法");
+        }
+    };
+    
+    window.MyPlugin = MyPlugin;
+    
+})();
+
+
+
